b80dd90edb716b4c7f016ba63391946d04719fcf
jtimm <jtimm@c661c0d1-8b1f-4263-bf69-2f84893fd559>
2011-05-23 19:32:13 +0000

    Ran model transformation. Re-generated model code. Code formatting and clean-up. Fix copyrights.

--- trunk/generated/org.openhealthtools.mdht.uml.cda.hitsp/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/ImmunizationsSectionOperations.java
+++ trunk/generated/org.openhealthtools.mdht.uml.cda.hitsp/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/ImmunizationsSectionOperations.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
+/*******************************************************************************
+ * Copyright (c) 2009, 2011 David A Carlson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id$
- */
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.hitsp.operations;
 
 import java.util.Collection;
@@ -198,7 +202,7 @@ public class ImmunizationsSectionOperations extends
 			OCL.Helper helper = EOCL_ENV.createOCLHelper();
 			helper.setOperationContext(
 				HITSPPackage.Literals.IMMUNIZATIONS_SECTION,
-				HITSPPackage.Literals.IMMUNIZATIONS_SECTION.getEAllOperations().get(65));
+				HITSPPackage.Literals.IMMUNIZATIONS_SECTION.getEAllOperations().get(71));
 			try {
 				GET_HITSP_IMMUNIZATIONS__EOCL_QRY = helper.createQuery(GET_HITSP_IMMUNIZATIONS__EOCL_EXP);
 			} catch (ParserException pe) {
--- trunk/generated/org.openhealthtools.mdht.uml.cda.hitsp/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/SurgeriesSectionOperations.java
+++ trunk/generated/org.openhealthtools.mdht.uml.cda.hitsp/src/org/openhealthtools/mdht/uml/cda/hitsp/operations/SurgeriesSectionOperations.java
@@ -1,9 +1,13 @@
-/**
- * <copyright>
- * </copyright>
+/*******************************************************************************
+ * Copyright (c) 2009, 2011 David A Carlson and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * $Id$
- */
+ * Contributors:
+ *     David A Carlson (XMLmodeling.com) - initial API and implementation
+ *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.hitsp.operations;
 
 import java.util.Collection;
@@ -198,7 +202,7 @@ public class SurgeriesSectionOperations extends CodedSurgeriesSectionOperations 
 			OCL.Helper helper = EOCL_ENV.createOCLHelper();
 			helper.setOperationContext(
 				HITSPPackage.Literals.SURGERIES_SECTION,
-				HITSPPackage.Literals.SURGERIES_SECTION.getEAllOperations().get(66));
+				HITSPPackage.Literals.SURGERIES_SECTION.getEAllOperations().get(67));
 			try {
 				GET_PROCEDURE_ACTIVITIES__EOCL_QRY = helper.createQuery(GET_PROCEDURE_ACTIVITIES__EOCL_EXP);
 			} catch (ParserException pe) {
