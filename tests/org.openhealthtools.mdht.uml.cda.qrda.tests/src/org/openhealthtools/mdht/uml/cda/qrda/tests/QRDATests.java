c630a44ebd07a720327a8e2e34121b20c0ac167f
seanmuir <seanmuir@c661c0d1-8b1f-4263-bf69-2f84893fd559>
2012-08-24 13:57:17 +0000

    Initial version of junit tests

---
+++ tests/org.openhealthtools.mdht.uml.cda.qrda.tests/src/org/openhealthtools/mdht/uml/cda/qrda/tests/QRDATests.java
@@ -0,0 +1,40 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.qrda.tests;
+
+
+import junit.framework.JUnit4TestAdapter;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>qrda</b></em>' package.
+ * <!-- end-user-doc -->
+ * 		
+
+ * @generated
+ */
+ @RunWith(Suite.class)
+@Suite.SuiteClasses({
+  	QualityReportingDocumentArchitectureDocumentTest.class
+  	,MeasureSectionTest.class
+})
+public class QRDATests {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter( QRDATests.class);
+	}
+	 
+
+} //QRDATests
---
+++ tests/org.openhealthtools.mdht.uml.cda.qrda.tests/src/org/openhealthtools/mdht/uml/cda/qrda/tests/qrdaAllTests.java
@@ -0,0 +1,50 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.openhealthtools.mdht.uml.cda.qrda.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test suite for the '<em><b>qrda</b></em>' model.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class qrdaAllTests extends TestSuite {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(suite());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static Test suite() {
+		TestSuite suite = new qrdaAllTests("qrda Tests");
+		suite.addTest(QRDATests.suite());
+		return suite;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public qrdaAllTests(String name) {
+		super(name);
+	}
+
+} //qrdaAllTests
