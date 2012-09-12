37b0734a5c5fe9e3ab40a339694075b62c58462b
rama.ramakrishnan <rama.ramakrishnan@c661c0d1-8b1f-4263-bf69-2f84893fd559>
2012-09-12 19:47:48 +0000

    synced with consol model changes and regen.. also fixed test cases..

--- trunk/generated/org.openhealthtools.mdht.uml.cda.mu2consol/src/org/openhealthtools/mdht/uml/cda/mu2consol/operations/GeneralHeaderConstraintsOperations.java
+++ trunk/generated/org.openhealthtools.mdht.uml.cda.mu2consol/src/org/openhealthtools/mdht/uml/cda/mu2consol/operations/GeneralHeaderConstraintsOperations.java
@@ -570,7 +570,7 @@ public class GeneralHeaderConstraintsOperations
 	 */
 	protected static final String VALIDATE_GENERAL_HEADER_CONSTRAINTS_RECORD_TARGET_PATIENT_ROLE_PATIENT_ETHNIC_GROUP_CODE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP = "self.recordTarget->excluding(null).patientRole->excluding(null).patient->excluding(null)->reject((ethnicGroupCode.oclIsUndefined() or ethnicGroupCode.isNullFlavorUndefined()) implies (not ethnicGroupCode.oclIsUndefined() and ethnicGroupCode.oclIsKindOf(datatypes::CE) and "+
 "let value : datatypes::CE = ethnicGroupCode.oclAsType(datatypes::CE) in "+
-"value.codeSystem = '2.16.840.1.113883.6.238' and not value.code.oclIsUndefined()))";
+"value.codeSystem = '2.16.840.1.113883.6.238' and (value.code = '2135-2' or value.code = '2186-5')))";
 
 	/**
 	 * The cached OCL invariant for the '{@link #validateGeneralHeaderConstraintsRecordTargetPatientRolePatientEthnicGroupCode(GeneralHeaderConstraints, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate General Header Constraints Record Target Patient Role Patient Ethnic Group Code</em>}' invariant operation.
