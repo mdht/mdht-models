497b454b91f5a27598135f1cc560961a5537957c
seanmuir <seanmuir@c661c0d1-8b1f-4263-bf69-2f84893fd559>
2012-12-21 15:51:46 +0000

    Initial version of simple MDHT CDA Consol Example

---
+++ trunk/examples/org.openhealthtools.mdht.cda.consol.example/src/org/openhealthtools/mdht/cda/consol/example/TestGeneralHeaderSample.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2012 Sean Muir and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sean Muir (JKM Software) - initial API and implementation
+ *******************************************************************************/
+package org.openhealthtools.mdht.cda.consol.example;
+
+import java.io.FileInputStream;
+
+import org.eclipse.emf.common.util.Diagnostic;
+import org.openhealthtools.mdht.uml.cda.consol.ConsolPackage;
+import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
+import org.openhealthtools.mdht.uml.cda.util.ValidationResult;
+
+public class TestGeneralHeaderSample {
+	public static void main(String[] args) throws Exception {
+		// must load Consol package to validate those templates
+		ConsolPackage.eINSTANCE.eClass();
+
+		// create a validation result object to collect diagnostics produced during validation
+		ValidationResult result = new ValidationResult();
+		CDAUtil.load(new FileInputStream("samples/GeneralHeader_sample.xml"), result);
+
+		System.out.println("\n***** Sample validation results *****");
+		for (Diagnostic diagnostic : result.getErrorDiagnostics()) {
+			System.out.println("ERROR: " + diagnostic.getMessage());
+		}
+		for (Diagnostic diagnostic : result.getWarningDiagnostics()) {
+			System.out.println("WARNING: " + diagnostic.getMessage());
+		}
+		for (Diagnostic diagnostic : result.getInfoDiagnostics()) {
+			System.out.println("INFO: " + diagnostic.getMessage());
+		}
+
+		System.out.println();
+		System.out.println("Number of Schema Validation Diagnostics: " + result.getSchemaValidationDiagnostics().size());
+		System.out.println("Number of EMF Resource Diagnostics: " + result.getEMFResourceDiagnostics().size());
+		System.out.println("Number of EMF Validation Diagnostics: " + result.getEMFValidationDiagnostics().size());
+		System.out.println("Number of Total Diagnostics: " + result.getAllDiagnostics().size());
+
+		if (!result.hasErrors()) {
+			System.out.println("Document is valid");
+		} else {
+			System.out.println("Document is invalid");
+		}
+	}
+}
