782814121f4d5cb5c46eef0d4bee3c8da5547388
jtimm <jtimm@c661c0d1-8b1f-4263-bf69-2f84893fd559>
2014-01-09 20:28:33 +0000

    Applied and tested all patches for [artf3745] and [artf3586].

--- generated/org.openhealthtools.mdht.uml.cda.consol/src/org/openhealthtools/mdht/uml/cda/consol/StudyAct.java
+++ generated/org.openhealthtools.mdht.uml.cda.consol/src/org/openhealthtools/mdht/uml/cda/consol/StudyAct.java
@@ -144,7 +144,7 @@ public interface StudyAct extends Act {
 	 * @param diagnostics The chain of diagnostics to which problems are to be appended.
 	 * @param context The cache of context-specific information.
 	 * <!-- end-model-doc -->
-	 * @model annotation="http://www.eclipse.org/uml2/1.1.0/GenModel body='(self.code.oclIsUndefined() or self.code.isNullFlavorUndefined()) implies (not self.code.oclIsUndefined() and self.code.oclIsKindOf(datatypes::CD) and \r\nlet value : datatypes::CD = self.code.oclAsType(datatypes::CD) in \r\nvalue.code = \'113014\' and value.codeSystem = \'1.2.840.10008.2.16.4\')'"
+	 * @model annotation="http://www.eclipse.org/uml2/1.1.0/GenModel body='(self.code.oclIsUndefined() or self.code.isNullFlavorUndefined()) implies (not self.code.oclIsUndefined() and self.code.oclIsKindOf(datatypes::CD) and \nlet value : datatypes::CD = self.code.oclAsType(datatypes::CD) in \nvalue.code = \'113014\' and value.codeSystem = \'1.2.840.10008.2.16.4\')'"
 	 * @generated
 	 */
 	boolean validateStudyActCode(DiagnosticChain diagnostics, Map<Object, Object> context);
