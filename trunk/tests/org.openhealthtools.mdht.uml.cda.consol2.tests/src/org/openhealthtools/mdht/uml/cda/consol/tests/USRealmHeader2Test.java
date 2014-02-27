39b63ef08ce4e7897935fbb830ac3dfe4b04ae35
drb.gfc <drb.gfc@c661c0d1-8b1f-4263-bf69-2f84893fd559>
2014-02-27 22:29:51 +0000

    As per artf3804 : Begin C-CDA R2 New Document Modeling and JUnit Coding: Added NEW Care Plan JUnit source code for all tests which did not generate valid code (which was most of them in this case). Known issue: The following 4 tests appear to be missing (they are not in ecore so it's expected but they are defined in the UML model so not being in ecore is not expected): testValidateCarePlanInformationRecipientIntendedRecipientReceivedOrganization testValidateCarePlanInformationRecipientIntendedRecipientReceivedOrganizationId testValidateCarePlanInformationRecipientIntendedRecipientReceivedOrganizationName testValidateCarePlanInformationRecipientIntendedRecipientReceivedOrganizationStandardIndustryClassCode The first 2 in the chain were generated (along with all other, unrelated tests): testValidateCarePlanInformationRecipientIntendedRecipient testValidateCarePlanInformationRecipient

---
+++ trunk/tests/org.openhealthtools.mdht.uml.cda.consol2.tests/src/org/openhealthtools/mdht/uml/cda/consol/tests/USRealmHeader2Test.java
@@ -0,0 +1,141 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Sean Muir, Dan Brown and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Sean Muir (JKM Software) and Dan Brown (Audacious Inquiry) - initial API and implementation
+ *     ? - additional testing code
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.cda.consol.tests;
+
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.consol.ConsolFactory;
+import org.openhealthtools.mdht.uml.cda.consol.USRealmHeader2;
+import org.openhealthtools.mdht.uml.cda.consol.operations.USRealmHeader2Operations;
+import org.openhealthtools.mdht.uml.cda.operations.CDAValidationTest;
+
+/**
+ * <!-- begin-user-doc -->
+ * A static utility class that provides operations related to '<em><b>US Realm Header2</b></em>' model objects.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following operations are supported:
+ * <ul>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.USRealmHeader2#validateGeneralHeaderConstraintsTemplateId(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate General Header Constraints Template Id</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+
+public class USRealmHeader2Test extends CDAValidationTest {
+
+	/**
+	*
+	* @generated
+	*/
+	@Test
+	public void testValidateGeneralHeaderConstraintsTemplateId() {
+		OperationsTestCase<USRealmHeader2> validateGeneralHeaderConstraintsTemplateIdTestCase = new OperationsTestCase<USRealmHeader2>(
+			"validateGeneralHeaderConstraintsTemplateId",
+			operationsForOCL.getOCLValue("VALIDATE_GENERAL_HEADER_CONSTRAINTS_TEMPLATE_ID__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(USRealmHeader2 target) {
+
+			}
+
+			@Override
+			protected void updateToPass(USRealmHeader2 target) {
+				target.init();
+
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return USRealmHeader2Operations.validateGeneralHeaderConstraintsTemplateId(
+					(USRealmHeader2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateGeneralHeaderConstraintsTemplateIdTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	private static class OperationsForOCL extends USRealmHeader2Operations {
+		public String getOCLValue(String fieldName) {
+
+			String oclValue = null;
+
+			try {
+				oclValue = (String) this.getClass().getSuperclass().getDeclaredField(fieldName).get(this);
+			} catch (Exception e) {
+				oclValue = "NO OCL FOUND FOR PROPERTY " + fieldName;
+			}
+			return oclValue;
+		}
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	private static class ObjectFactory implements TestObjectFactory<USRealmHeader2> {
+		public USRealmHeader2 create() {
+			return ConsolFactory.eINSTANCE.createUSRealmHeader2();
+		}
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	private static OperationsForOCL operationsForOCL = new OperationsForOCL();
+
+	/**
+	*
+	* @generated
+	*/
+	private static ObjectFactory objectFactory = new ObjectFactory();
+
+	/**
+	* Tests Operations Constructor for 100% coverage
+	* @generated
+	*/
+	private static class ConstructorTestClass extends USRealmHeader2Operations {
+	};
+
+	/**
+	* Tests Operations Constructor for 100% coverage
+	* @generated
+	*/
+	@Test
+	public void testConstructor() {
+		@SuppressWarnings("unused")
+		ConstructorTestClass constructorTestClass = new ConstructorTestClass();
+	} // testConstructor
+
+	/**
+	*
+	* @generated
+	*/
+	@Override
+	protected EObject getObjectToTest() {
+		return null;
+	}
+
+} // USRealmHeader2Operations
--- trunk/tests/org.openhealthtools.mdht.uml.cda.consol2.tests/src/org/openhealthtools/mdht/uml/cda/consol/tests/WoundObservationTest.java
+++ trunk/tests/org.openhealthtools.mdht.uml.cda.consol2.tests/src/org/openhealthtools/mdht/uml/cda/consol/tests/WoundObservationTest.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2014 Sean Muir, Dan Brown and others.
+ * Copyright (c) 2014 Dan Brown and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Sean Muir (JKM Software) and Dan Brown (Audacious Inquiry) - initial API and implementation
- *     Dan Brown - additional testing code
+ *     Dan Brown (Audacious Inquiry) - initial API and implementation
+ *									 - additional testing code
  *******************************************************************************/
 package org.openhealthtools.mdht.uml.cda.consol.tests;
 
