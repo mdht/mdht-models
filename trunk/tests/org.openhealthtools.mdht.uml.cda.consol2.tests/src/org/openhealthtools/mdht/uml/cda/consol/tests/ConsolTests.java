2e402211f1b162249b1ce704eb832c5f42b30382
drb.gfc <drb.gfc@c661c0d1-8b1f-4263-bf69-2f84893fd559>
2014-03-07 22:18:06 +0000

    As per artf3804 : Begin C-CDA R2 New Document Modeling and JUnit Coding: Added NEW Transfer Summary JUnit source code for all tests which did not generate valid code (which was most of them in this case). -Slight string change in Care Plan

--- trunk/tests/org.openhealthtools.mdht.uml.cda.consol2.tests/src/org/openhealthtools/mdht/uml/cda/consol/tests/ConsolTests.java
+++ trunk/tests/org.openhealthtools.mdht.uml.cda.consol2.tests/src/org/openhealthtools/mdht/uml/cda/consol/tests/ConsolTests.java
@@ -97,7 +97,7 @@ import org.junit.runners.Suite;
 		WoundMeasurementObservationTest.class, WoundObservationTest.class, GoalsSectionTest.class,
 		HealthConcernsSectionTest.class, HealthStatusEvaluationsOutcomesSectionTest.class,
 		MentalStatusSectionTest.class, NutritionSectionTest.class, PhysicalFindingsOfSkinSectionTest.class,
-		CarePlanTest.class, ReferralNoteTest.class })
+		CarePlanTest.class, ReferralNoteTest.class, TransferSummaryTest.class })
 public class ConsolTests {
 
 	/**
