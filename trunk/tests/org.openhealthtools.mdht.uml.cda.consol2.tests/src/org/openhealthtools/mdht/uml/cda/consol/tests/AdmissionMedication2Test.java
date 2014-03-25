dd30b91464414e6079167c570cbd82c85ae1914d
drb.gfc <drb.gfc@c661c0d1-8b1f-4263-bf69-2f84893fd559>
2014-03-25 00:48:56 +0000

    JUnit code for early versions of: AdmissionMedication2 AdvanceDirectiveObservation2 AllergyConcernAct2

---
+++ trunk/tests/org.openhealthtools.mdht.uml.cda.consol2.tests/src/org/openhealthtools/mdht/uml/cda/consol/tests/AdmissionMedication2Test.java
@@ -0,0 +1,219 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Dan Brown and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dan Brown (Audacious Inquiry) - initial API and implementation
+ *									 - additional testing code
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.cda.consol.tests;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.consol.AdmissionMedication2;
+import org.openhealthtools.mdht.uml.cda.consol.ConsolFactory;
+import org.openhealthtools.mdht.uml.cda.consol.operations.AdmissionMedication2Operations;
+import org.openhealthtools.mdht.uml.cda.operations.CDAValidationTest;
+
+/**
+ * <!-- begin-user-doc -->
+ * A static utility class that provides operations related to '<em><b>Admission Medication2</b></em>' model objects.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following operations are supported:
+ * <ul>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AdmissionMedication2#validateAdmissionMedicationTemplateId(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Admission Medication Template Id</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AdmissionMedication2#validateAdmissionMedicationCodeP(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Admission Medication Code P</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AdmissionMedication2#validateAdmissionMedicationCode(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Admission Medication Code</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+
+public class AdmissionMedication2Test extends CDAValidationTest {
+
+	/**
+	*
+	* @generated
+	*/
+	@Test
+	public void testValidateAdmissionMedicationTemplateId() {
+		OperationsTestCase<AdmissionMedication2> validateAdmissionMedicationTemplateIdTestCase = new OperationsTestCase<AdmissionMedication2>(
+			"validateAdmissionMedicationTemplateId",
+			operationsForOCL.getOCLValue("VALIDATE_ADMISSION_MEDICATION_TEMPLATE_ID__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AdmissionMedication2 target) {
+
+			}
+
+			@Override
+			protected void updateToPass(AdmissionMedication2 target) {
+				target.init();
+
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AdmissionMedication2Operations.validateAdmissionMedicationTemplateId(
+					(AdmissionMedication2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAdmissionMedicationTemplateIdTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	@Test
+	public void testValidateAdmissionMedicationCodeP() {
+		OperationsTestCase<AdmissionMedication2> validateAdmissionMedicationCodePTestCase = new OperationsTestCase<AdmissionMedication2>(
+			"validateAdmissionMedicationCodeP",
+			operationsForOCL.getOCLValue("VALIDATE_ADMISSION_MEDICATION_CODE_P__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AdmissionMedication2 target) {
+
+			}
+
+			@Override
+			protected void updateToPass(AdmissionMedication2 target) {
+				target.init();
+
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AdmissionMedication2Operations.validateAdmissionMedicationCodeP(
+					(AdmissionMedication2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAdmissionMedicationCodePTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	@Test
+	public void testValidateAdmissionMedicationCode() {
+		OperationsTestCase<AdmissionMedication2> validateAdmissionMedicationCodeTestCase = new OperationsTestCase<AdmissionMedication2>(
+			"validateAdmissionMedicationCode",
+			operationsForOCL.getOCLValue("VALIDATE_ADMISSION_MEDICATION_CODE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AdmissionMedication2 target) {
+
+			}
+
+			@Override
+			protected void updateToPass(AdmissionMedication2 target) {
+				target.init();
+
+			}
+
+			@Override
+			protected void setDependency(AdmissionMedication2 target) {
+				Collection<Object> passToken = new java.util.ArrayList<Object>(3);
+				passToken.add(target);
+				map.put("org.openhealthtools.mdht.uml.cda.consol.AdmissionMedicationCodeP", passToken);
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AdmissionMedication2Operations.validateAdmissionMedicationCode(
+					(AdmissionMedication2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAdmissionMedicationCodeTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	private static class OperationsForOCL extends AdmissionMedication2Operations {
+		public String getOCLValue(String fieldName) {
+
+			String oclValue = null;
+
+			try {
+				oclValue = (String) this.getClass().getSuperclass().getDeclaredField(fieldName).get(this);
+			} catch (Exception e) {
+				oclValue = "NO OCL FOUND FOR PROPERTY " + fieldName;
+			}
+			return oclValue;
+		}
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	private static class ObjectFactory implements TestObjectFactory<AdmissionMedication2> {
+		public AdmissionMedication2 create() {
+			return ConsolFactory.eINSTANCE.createAdmissionMedication2();
+		}
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	private static OperationsForOCL operationsForOCL = new OperationsForOCL();
+
+	/**
+	*
+	* @generated
+	*/
+	private static ObjectFactory objectFactory = new ObjectFactory();
+
+	/**
+	* Tests Operations Constructor for 100% coverage
+	* @generated
+	*/
+	private static class ConstructorTestClass extends AdmissionMedication2Operations {
+	};
+
+	/**
+	* Tests Operations Constructor for 100% coverage
+	* @generated
+	*/
+	@Test
+	public void testConstructor() {
+		@SuppressWarnings("unused")
+		ConstructorTestClass constructorTestClass = new ConstructorTestClass();
+	} // testConstructor
+
+	/**
+	*
+	* @generated
+	*/
+	@Override
+	protected EObject getObjectToTest() {
+		return null;
+	}
+
+} // AdmissionMedication2Operations
---
+++ trunk/tests/org.openhealthtools.mdht.uml.cda.consol2.tests/src/org/openhealthtools/mdht/uml/cda/consol/tests/AllergyConcernAct2Test.java
@@ -0,0 +1,367 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Dan Brown and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dan Brown (Audacious Inquiry) - initial API and implementation
+ *									 - additional testing code
+ *******************************************************************************/
+package org.openhealthtools.mdht.uml.cda.consol.tests;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.ecore.EObject;
+import org.junit.Test;
+import org.openhealthtools.mdht.uml.cda.consol.AllergyConcernAct2;
+import org.openhealthtools.mdht.uml.cda.consol.ConsolFactory;
+import org.openhealthtools.mdht.uml.cda.consol.operations.AllergyConcernAct2Operations;
+import org.openhealthtools.mdht.uml.cda.operations.CDAValidationTest;
+import org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesFactory;
+import org.openhealthtools.mdht.uml.hl7.datatypes.IVL_TS;
+import org.openhealthtools.mdht.uml.hl7.datatypes.IVXB_TS;
+
+/**
+ * <!-- begin-user-doc -->
+ * A static utility class that provides operations related to '<em><b>Allergy Concern Act2</b></em>' model objects.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following operations are supported:
+ * <ul>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AllergyConcernAct2#validateAllergyProblemActCodeP(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Allergy Problem Act Code P</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AllergyConcernAct2#validateAllergyConcernAct2AuthorParticipation(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Allergy Concern Act2 Author Participation</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AllergyConcernAct2#validateAllergyConcernAct2IVLTSLow(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Allergy Concern Act2 IVLTS Low</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AllergyConcernAct2#validateAllergyConcernAct2IVLTSHigh(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Allergy Concern Act2 IVLTS High</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AllergyConcernAct2#validateAllergyProblemActTemplateId(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Allergy Problem Act Template Id</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AllergyConcernAct2#validateAllergyProblemActCode(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Allergy Problem Act Code</em>}</li>
+ *   <li>{@link org.openhealthtools.mdht.uml.cda.consol.AllergyConcernAct2#validateAllergyProblemActEffectiveTime(org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Allergy Problem Act Effective Time</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+
+public class AllergyConcernAct2Test extends CDAValidationTest {
+
+	/**
+	*
+	* @generated
+	*/
+	@Test
+	public void testValidateAllergyProblemActCodeP() {
+		OperationsTestCase<AllergyConcernAct2> validateAllergyProblemActCodePTestCase = new OperationsTestCase<AllergyConcernAct2>(
+			"validateAllergyProblemActCodeP",
+			operationsForOCL.getOCLValue("VALIDATE_ALLERGY_PROBLEM_ACT_CODE_P__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AllergyConcernAct2 target) {
+
+			}
+
+			@Override
+			protected void updateToPass(AllergyConcernAct2 target) {
+				target.init();
+
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AllergyConcernAct2Operations.validateAllergyProblemActCodeP(
+					(AllergyConcernAct2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAllergyProblemActCodePTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated NOT
+	*/
+	@Test
+	public void testValidateAllergyConcernAct2AuthorParticipation() {
+		OperationsTestCase<AllergyConcernAct2> validateAllergyConcernAct2AuthorParticipationTestCase = new OperationsTestCase<AllergyConcernAct2>(
+			"validateAllergyConcernAct2AuthorParticipation",
+			operationsForOCL.getOCLValue("VALIDATE_ALLERGY_CONCERN_ACT2_AUTHOR_PARTICIPATION__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AllergyConcernAct2 target) {
+				target.init();
+			}
+
+			@Override
+			protected void updateToPass(AllergyConcernAct2 target) {
+				target.getAuthors().add(ConsolFactory.eINSTANCE.createAuthorParticipation().init());
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AllergyConcernAct2Operations.validateAllergyConcernAct2AuthorParticipation(
+					(AllergyConcernAct2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAllergyConcernAct2AuthorParticipationTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated NOT
+	*/
+	@Test
+	public void testValidateAllergyConcernAct2IVLTSLow() {
+		OperationsTestCase<AllergyConcernAct2> validateAllergyConcernAct2IVLTSLowTestCase = new OperationsTestCase<AllergyConcernAct2>(
+			"validateAllergyConcernAct2IVLTSLow",
+			operationsForOCL.getOCLValue("VALIDATE_ALLERGY_CONCERN_ACT2_IVLTS_LOW__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AllergyConcernAct2 target) {
+				target.init();
+				IVL_TS ts = DatatypesFactory.eINSTANCE.createIVL_TS();
+				target.setEffectiveTime(ts);
+			}
+
+			@Override
+			protected void updateToPass(AllergyConcernAct2 target) {
+				IVXB_TS low = DatatypesFactory.eINSTANCE.createIVXB_TS();
+				low.setValue(PRECISE_TO_DAY);
+				target.getEffectiveTime().setLow(low);
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AllergyConcernAct2Operations.validateAllergyConcernAct2IVLTSLow(
+					(AllergyConcernAct2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAllergyConcernAct2IVLTSLowTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated NOT
+	*/
+	@Test
+	public void testValidateAllergyConcernAct2IVLTSHigh() {
+		OperationsTestCase<AllergyConcernAct2> validateAllergyConcernAct2IVLTSHighTestCase = new OperationsTestCase<AllergyConcernAct2>(
+			"validateAllergyConcernAct2IVLTSHigh",
+			operationsForOCL.getOCLValue("VALIDATE_ALLERGY_CONCERN_ACT2_IVLTS_HIGH__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AllergyConcernAct2 target) {
+				target.init();
+				IVL_TS ts = DatatypesFactory.eINSTANCE.createIVL_TS();
+				target.setEffectiveTime(ts);
+			}
+
+			@Override
+			protected void updateToPass(AllergyConcernAct2 target) {
+				target.getEffectiveTime().setHigh(DatatypesFactory.eINSTANCE.createIVXB_TS());
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AllergyConcernAct2Operations.validateAllergyConcernAct2IVLTSHigh(
+					(AllergyConcernAct2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAllergyConcernAct2IVLTSHighTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	@Test
+	public void testValidateAllergyProblemActTemplateId() {
+		OperationsTestCase<AllergyConcernAct2> validateAllergyProblemActTemplateIdTestCase = new OperationsTestCase<AllergyConcernAct2>(
+			"validateAllergyProblemActTemplateId",
+			operationsForOCL.getOCLValue("VALIDATE_ALLERGY_PROBLEM_ACT_TEMPLATE_ID__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AllergyConcernAct2 target) {
+
+			}
+
+			@Override
+			protected void updateToPass(AllergyConcernAct2 target) {
+				target.init();
+
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AllergyConcernAct2Operations.validateAllergyProblemActTemplateId(
+					(AllergyConcernAct2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAllergyProblemActTemplateIdTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated NOT
+	*/
+	@Test
+	public void testValidateAllergyProblemActCode() {
+		OperationsTestCase<AllergyConcernAct2> validateAllergyProblemActCodeTestCase = new OperationsTestCase<AllergyConcernAct2>(
+			"validateAllergyProblemActCode",
+			operationsForOCL.getOCLValue("VALIDATE_ALLERGY_PROBLEM_ACT_CODE__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AllergyConcernAct2 target) {
+				target.init();
+			}
+
+			@Override
+			protected void updateToPass(AllergyConcernAct2 target) {
+				target.setCode(DatatypesFactory.eINSTANCE.createCD("CONC", "2.16.840.1.113883.5.6"));
+			}
+
+			@Override
+			protected void setDependency(AllergyConcernAct2 target) {
+				Collection<Object> passToken = new java.util.ArrayList<Object>(3);
+				passToken.add(target);
+				map.put("org.openhealthtools.mdht.uml.cda.consol.AllergyProblemActCodeP", passToken);
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AllergyConcernAct2Operations.validateAllergyProblemActCode(
+					(AllergyConcernAct2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAllergyProblemActCodeTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	@Test
+	public void testValidateAllergyProblemActEffectiveTime() {
+		OperationsTestCase<AllergyConcernAct2> validateAllergyProblemActEffectiveTimeTestCase = new OperationsTestCase<AllergyConcernAct2>(
+			"validateAllergyProblemActEffectiveTime",
+			operationsForOCL.getOCLValue("VALIDATE_ALLERGY_PROBLEM_ACT_EFFECTIVE_TIME__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP"),
+			objectFactory) {
+
+			@Override
+			protected void updateToFail(AllergyConcernAct2 target) {
+
+			}
+
+			@Override
+			protected void updateToPass(AllergyConcernAct2 target) {
+				target.init();
+
+				IVL_TS ts = DatatypesFactory.eINSTANCE.createIVL_TS();
+				target.setEffectiveTime(ts);
+
+			}
+
+			@Override
+			protected boolean validate(EObject objectToTest, BasicDiagnostic diagnostician, Map<Object, Object> map) {
+
+				return AllergyConcernAct2Operations.validateAllergyProblemActEffectiveTime(
+					(AllergyConcernAct2) objectToTest, diagnostician, map);
+			}
+
+		};
+
+		validateAllergyProblemActEffectiveTimeTestCase.doValidationTest();
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	private static class OperationsForOCL extends AllergyConcernAct2Operations {
+		public String getOCLValue(String fieldName) {
+
+			String oclValue = null;
+
+			try {
+				oclValue = (String) this.getClass().getSuperclass().getDeclaredField(fieldName).get(this);
+			} catch (Exception e) {
+				oclValue = "NO OCL FOUND FOR PROPERTY " + fieldName;
+			}
+			return oclValue;
+		}
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	private static class ObjectFactory implements TestObjectFactory<AllergyConcernAct2> {
+		public AllergyConcernAct2 create() {
+			return ConsolFactory.eINSTANCE.createAllergyConcernAct2();
+		}
+	}
+
+	/**
+	*
+	* @generated
+	*/
+	private static OperationsForOCL operationsForOCL = new OperationsForOCL();
+
+	/**
+	*
+	* @generated
+	*/
+	private static ObjectFactory objectFactory = new ObjectFactory();
+
+	/**
+	* Tests Operations Constructor for 100% coverage
+	* @generated
+	*/
+	private static class ConstructorTestClass extends AllergyConcernAct2Operations {
+	};
+
+	/**
+	* Tests Operations Constructor for 100% coverage
+	* @generated
+	*/
+	@Test
+	public void testConstructor() {
+		@SuppressWarnings("unused")
+		ConstructorTestClass constructorTestClass = new ConstructorTestClass();
+	} // testConstructor
+
+	/**
+	*
+	* @generated
+	*/
+	@Override
+	protected EObject getObjectToTest() {
+		return null;
+	}
+
+} // AllergyConcernAct2Operations
