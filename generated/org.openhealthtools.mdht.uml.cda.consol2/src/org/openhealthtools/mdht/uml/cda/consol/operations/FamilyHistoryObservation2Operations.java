3d1000bfd8c83efb3f9b676441ae6293addbe177
drbgfc <drb.gfc@comcast.net>
2015-09-22 18:12:39 -0400

    Update extension for Family History Observation 2

--- generated/org.openhealthtools.mdht.uml.cda.consol2/src/org/openhealthtools/mdht/uml/cda/consol/operations/FamilyHistoryObservation2Operations.java
+++ generated/org.openhealthtools.mdht.uml.cda.consol2/src/org/openhealthtools/mdht/uml/cda/consol/operations/FamilyHistoryObservation2Operations.java
@@ -194,7 +194,7 @@ public class FamilyHistoryObservation2Operations extends FamilyHistoryObservatio
 	 * @generated
 	 * @ordered
 	 */
-	protected static final String VALIDATE_FAMILY_HISTORY_OBSERVATION_TEMPLATE_ID__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP = "self.templateId->exists(id : datatypes::II | id.root = '2.16.840.1.113883.10.20.22.4.46' and id.extension = '2014-06-09')";
+	protected static final String VALIDATE_FAMILY_HISTORY_OBSERVATION_TEMPLATE_ID__DIAGNOSTIC_CHAIN_MAP__EOCL_EXP = "self.templateId->exists(id : datatypes::II | id.root = '2.16.840.1.113883.10.20.22.4.46' and id.extension = '2015-08-01')";
 
 	/**
 	 * The cached OCL invariant for the '{@link #validateFamilyHistoryObservationTemplateId(FamilyHistoryObservation2, org.eclipse.emf.common.util.DiagnosticChain, java.util.Map) <em>Validate Family History Observation Template Id</em>}' invariant operation.
